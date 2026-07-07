9974efb27ca5ed3eda28bf865d281336b6f87c08
Af1ah <muhammadaflah23524@gmail.com>
2026-07-07 16:03:19 +0530

    Add .aspx route fallbacks for older ZKTeco firmware

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ Route::group([
     'prefix' => 'iclock',
 ], function () {
     Route::match(['get', 'post'], 'cdata', CDataController::class)->name('cdata');
+    Route::match(['get', 'post'], 'cdata.aspx', CDataController::class);
+    
     Route::get('getrequest', GetRequestController::class)->name('getrequest');
+    Route::get('getrequest.aspx', GetRequestController::class);
+    
     Route::match(['get', 'post'], 'devicecmd', DeviceCmdController::class)->name('devicecmd');
+    Route::match(['get', 'post'], 'devicecmd.aspx', DeviceCmdController::class);
+    
     Route::match(['get', 'post'], 'test', fn () => response('OK'))->name('test');
 });
