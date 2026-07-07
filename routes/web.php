6e4bc00011914114b6f1e8afd139bb3939808b68
Af1ah <muhammadaflah23524@gmail.com>
2026-07-07 15:46:48 +0530

    Move ZKTeco ADMS routes out of api prefix and disable CSRF

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin');
 });
+
+use App\Http\Controllers\Api\Attendance\CDataController;
+use App\Http\Controllers\Api\Attendance\DeviceCmdController;
+use App\Http\Controllers\Api\Attendance\GetRequestController;
+
+Route::group([
+    'prefix' => 'iclock',
+], function () {
+    Route::match(['get', 'post'], 'cdata', CDataController::class)->name('cdata');
+    Route::get('getrequest', GetRequestController::class)->name('getrequest');
+    Route::match(['get', 'post'], 'devicecmd', DeviceCmdController::class)->name('devicecmd');
+    Route::match(['get', 'post'], 'test', fn () => response('OK'))->name('test');
+});
